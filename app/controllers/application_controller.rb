2c983fe9bd797e4286c0ebd766fa4ec82b04db37
Fable42 <kovalskii.4@mail.ru>
2024-02-29 17:56:00 +0300

    Added auth (without styles)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :new_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def new_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :tag])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :tag])
+  end
 end
