e88cdfd6f988cfc33666e69fd1a802f11fb049ba
Fable42 <kovalskii.4@mail.ru>
2024-03-05 16:02:08 +0300

    Added associations, added auto redirect for non signed users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  resources :posts, only: %i[new create index update edit destroy]
+  resources :posts
+  #resources :posts, only: %i[new create index update edit destroy]
 
   #resources :posts, only: [:new, :update, :edit, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
