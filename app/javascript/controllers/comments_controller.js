c03561c27db577fc90623181a966032b4f0040f4
Fable42 <kovalskii.4@mail.ru>
2024-03-16 23:06:28 +0300

    add nessted comments

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -16,4 +16,36 @@ export default class extends Controller {
     const commentBody = document.getElementById(commentBodyID);
     commentBody.classList.toggle("d-none");
   }
+
+  reply(event) {
+    event.preventDefault();
+
+    // Получаем данные из data-атрибутов
+    const commentId = event.currentTarget.dataset.commentId;
+    const userName = event.currentTarget.dataset.userName;
+
+    // Находим поля формы и устанавливаем значения
+    const commentIdField = document.getElementById('parent_id_field');
+    commentIdField.value = commentId;
+    const userNameDiv = document.getElementById('user_name');
+    userNameDiv.textContent = userName;
+    const visibleDiv = document.getElementById('reply_to');
+    visibleDiv.classList.toggle('d-none');
+
+    // Устанавливаем фокус на форму
+    const form = document.getElementById('comment_form');
+    form.focus();
+  }
+
+  cancel(event) {
+    event.preventDefault();
+
+    // Скрываем другой div
+    const visibleDiv = document.getElementById('reply_to');
+    visibleDiv.classList.add('d-none');
+
+    // Очищаем поле ввода
+    const form = document.getElementById('comment_form');
+    form.reset();
+  }
 }
