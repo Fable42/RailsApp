333089d1cd5d9937196ef6ce069038a30b5f2fd9
Fable42 <kovalskii.4@mail.ru>
2024-03-22 21:37:17 +0300

    add controlller and turbo frame for pagination

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -1,5 +1,8 @@
 class MainPagesController < ApplicationController
   def index
-    @posts = Post.all
+    #@posts = Post.all
+
+    @page = params[:page] || 1 
+    @posts = Post.page @page
   end
 end
