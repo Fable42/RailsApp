6ddf6c413ddc87057f4daeef5af0e4c218e08afe
Fable42 <kovalskii.4@mail.ru>
2024-04-05 23:49:43 +0300

    Added turbo to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,31 @@
 class CommentsController < ApplicationController
-  before_action :set_post, only: %i[destroy]
+  before_action :set_post
 
   def create
     @comment = current_user.comments.create(comment_params)
+    
+    respond_to do |format|
 
-    redirect_to post_path(params[:post_id]), notice: 'Comment created'
+      if request.referer.ends_with? post_path(@post)
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.replace("comment_form", partial: "comments/form-new", locals: { post: @post}),
+            turbo_stream.append("comments", partial: "comments/comment", locals: { comment: @comment })
+          ]
+        end
+      else
+        format.html { redirect_to post_path(@post), notice: "New comment created!" }
+      end
+    end
   end
 
   def destroy
     @comment = @post.comments.find(params[:id])
     @comment.destroy
 
-    redirect_to post_path(params[:post_id]), notice: 'Comment deleted'
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("comment-#{@comment.id}") }
+    end    
   end
 
   private 
